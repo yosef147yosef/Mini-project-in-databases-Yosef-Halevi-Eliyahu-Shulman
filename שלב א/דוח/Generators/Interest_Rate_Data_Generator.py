41e00256e20f552abda87906621e28a9e05595c9
EliyahuShulman <126463473+EliyahuShulman@users.noreply.github.com>
2024-05-24 12:00:51 +0300

    Update Interest_Rate_Data_Generator.py

--- שלב א/דוח/Generators/Interest_Rate_Data_Generator.py
+++ שלב א/דוח/Generators/Interest_Rate_Data_Generator.py
@@ -1,26 +1,18 @@
-import pandas as pd
 import random
 from datetime import datetime, timedelta
 
 def generate_interest_rate_data(num_records):
-    interest_rate_data = []
+    interest_rate_data = """Interest_Rate_ID,Benefits,Type,Prime,Interest
+"""
     for i in range(num_records):
-        interest_rate_id = i
+        interest_rate_id = str(i+1)
         benefits = random.choice(["None", "Long-Term Customer", "Soldier", "Student"])
         interest_type = random.choice(["Short-term", "Long-term"])
-        prime = round(random.uniform(0, 10), 2)
-        interest = round(random.uniform(0, 10), 2)
-        interest_rate_data.append((str(interest_rate_id), benefits, interest_type, prime, interest))
+        prime = str(round(random.uniform(0, 10), 2))
+        interest = str(round(random.uniform(0, 10), 2))
+        interest_rate_data+=interest_rate_id + "," + benefits + "," + interest_type + "," + prime + "," + interest + "\n"
     return interest_rate_data
 
 num_interest_rate_records = 500
 
-interest_rate_data = generate_interest_rate_data(num_interest_rate_records)
-
-columns = ['Interest_Rate_ID', 'Benefits', 'Type', 'Prime', 'Interest']
-interest_rate_df = pd.DataFrame(interest_rate_data, columns=columns)
-
-excel_file_path = "interest_rates.xlsx"
-interest_rate_df.to_excel(excel_file_path, index=False)
-
-print(f"Interest rate data has been exported to {excel_file_path}")
+print(generate_interest_rate_data(num_interest_rate_records))
