aab3fe92493203d25a8b5d261f4bb3d915978a4f
EliyahuShulman <126463473+EliyahuShulman@users.noreply.github.com>
2024-06-16 13:25:12 +0300

    Create func1.sql

---
+++ שלב ג/func1.sql
@@ -0,0 +1,16 @@
+-- Returns Balance-Amount if Balance is greater than amount. Otherwise it will return 0.
+DROP FUNCTION IF EXISTS func1;
+DELIMITER $$
+CREATE FUNCTION func1(Balance FLOAT, Amount FLOAT) RETURNS DECIMAL(9,2)
+BEGIN
+  DECLARE LeftOver DECIMAL(9,2);
+  SET LeftOver = 0;
+  IF Balance>Amount THEN
+  SET LeftOver = Balance-Amount;
+  END IF;
+  RETURN LeftOver;
+END$$
+DELIMITER ;
+SELECT
+func(Balance,Amount),Balance,Amount
+FROM deposit NATURAL JOIN account;
