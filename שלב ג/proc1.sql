9b4754a6f2cddf8c000f0a069f49f55b9b2d591d
EliyahuShulman <126463473+EliyahuShulman@users.noreply.github.com>
2024-06-16 13:26:09 +0300

    Update proc1.sql

--- שלב ג/proc1.sql
+++ שלב ג/proc1.sql
@@ -1,1 +1,23 @@
-
+-- Adds a worker by the name of "Andy" with the id given into the worker table. If it is null or already exists in the table the name "Andy" will be added with the minimal id that does not already exist in the table.
+DROP PROCEDURE IF EXISTS proc1;
+DELIMITER $$
+CREATE PROCEDURE proc1(NewID INT)
+BEGIN
+DECLARE minID INT;
+DECLARE CONTINUE HANDLER FOR 1048,1062
+BEGIN
+SET minID = 0;
+label: LOOP
+    SET minID = minID +1;
+    select minID ;
+    IF NOT EXISTS(SELECT * FROM worker WHERE worker.Worker_ID = minID) THEN
+     LEAVE label;
+    END IF;
+ END LOOP label;
+    INSERT INTO worker
+	VALUES (minID, "Andy");
+END ;
+INSERT INTO worker
+VALUES (NewID, "Andy");
+END$$
+DELIMITER ;
